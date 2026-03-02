6dc6bc3fb9db49ebaa9fe0676787e2e4194a043c
harshil bhatt <promme2020@gmail.com>
2026-03-01 17:44:00 -0800

    update

--- pe.py
+++ pe.py
@@ -9,7 +9,7 @@
 # git add . && git commit -m "update" && git push
 import sys
 from codex_python_sdk import create_client
-# make sure to import right
+# make sure to import right 
 # Get prompt from command line; default if none provided
 prompte = sys.argv[1] if len(sys.argv) > 1 else "Open chrome"
 print("Your code will be executed: " , prompte)
