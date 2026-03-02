9366a53e1c12393a08b5984edf9453d525efc484
harshil bhatt <promme2020@gmail.com>
2026-03-01 17:40:19 -0800

    update

--- pe.py
+++ pe.py
@@ -9,7 +9,7 @@
 
 import sys
 from codex_python_sdk import create_client
-
+# make sure to import right
 # Get prompt from command line; default if none provided
 prompte = sys.argv[1] if len(sys.argv) > 1 else "Open chrome"
 print("Your code will be executed: " , prompte)
